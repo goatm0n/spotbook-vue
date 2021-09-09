db080b89f99dc490b51a99c6d7827c432e5d30d0
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2021-09-09 11:53:51 +0100

    fix paths

--- spotbook-vueTerminal.bat
+++ spotbook-vueTerminal.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\User\Documents\GitHub\spotbook-vue\venv\scripts & activate.bat & cd /d C:\Users\User\Documents\GitHub\spotbook\mydjangos\spotbook & python manage.py shell"
+cmd /k "cd /d C:\Users\User\Documents\GitHub\spotbook-vue\venv\scripts & activate.bat & cd /d C:\Users\User\Documents\GitHub\spotbook-vue\mydjangos\spotbook & python manage.py shell"
